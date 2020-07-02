b12d531a4c3da7f41282d3ba11b21ffd588dce4d
K. S. Ernest (iFIre) Lee <ernest.lee@chibifire.com>
2020-07-01 22:44:16 -0700

    Add marker trim.

--- lottie_composition.h
+++ lottie_composition.h
@@ -15,6 +15,7 @@ protected:
 public:
   LottieFont() {}
 };
+
 class LottieImageAsset : public Resource {
   GDCLASS(LottieImageAsset, Resource);
 
@@ -24,24 +25,42 @@ protected:
 public:
   LottieImageAsset() {}
 };
-class Layer : public Resource {
-  GDCLASS(Layer, Resource);
+
+class LottieLayer : public Resource {
+  GDCLASS(LottieLayer, Resource);
 
 protected:
   static void _bind_methods() {}
 
 public:
-  Layer() {}
+  LottieLayer() {}
 };
-class Marker : public Resource {
-  GDCLASS(Marker, Resource);
+
+class LottieMarker : public Resource {
+  GDCLASS(LottieMarker, Resource);
+
+  String name;
+  float start_frame;
+  float duration_frames;
 
 protected:
   static void _bind_methods() {}
 
 public:
-  Marker() {}
+  bool matches_name(String p_name) {
+    if (p_name == name.to_lower()) {
+      return true;
+    }
+
+    // Trim the name to prevent confusion in design.
+    if (p_name == name.to_lower().strip_edges(false, true)) {
+      return true;
+    }
+    return false;
+  }
+  LottieMarker(){};
 };
+
 class LottieFontCharacter : public Resource {
   GDCLASS(LottieFontCharacter, Resource);
 
@@ -51,9 +70,10 @@ protected:
 public:
   LottieFontCharacter() {}
 };
+
 /**
- * After Effects/Bodymovin composition model. This is the serialized model from
- * which the animation will be created.
+ * After Effects/Bodymovin composition model. This is the serialized model
+ * from which the animation will be created.
  *
  * Based on https://github.com/airbnb/lottie-android
  */
@@ -61,14 +81,14 @@ class LottieComposition : public Resource {
   GDCLASS(LottieComposition, Resource);
   // PerformanceTracker performanceTracker = new PerformanceTracker();
   Set<String> warnings;
-  Map<String, Vector<Layer>> precomps;
+  Map<String, Vector<LottieLayer>> precomps;
   Map<String, LottieImageAsset> images;
   /** Map of font names to fonts */
   Map<String, LottieFont> fonts;
-  Vector<Marker> markers;
+  Vector<LottieMarker> markers;
   Vector<LottieFontCharacter> characters;
-  Vector<Layer> layer_map;
-  Vector<Layer> layers;
+  Vector<LottieLayer> layer_map;
+  Vector<LottieLayer> layers;
   Rect2 bounds;
   float start_frame;
   float end_frame;
@@ -85,6 +105,7 @@ public:
     Ref<JSONParseResult> result = _JSON::get_singleton()->parse(json);
     ERR_FAIL_COND_V(result.is_null(), Error::ERR_PARSE_ERROR);
     Dictionary data = result->get_result();
+
     return OK;
   }
   float get_duration_frames() { return end_frame - start_frame; }
