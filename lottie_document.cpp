00d2678a2e4cdce31cfc05b4b28449684064739e
K. S. Ernest (iFire) Lee <ernest.lee@chibifire.com>
2020-07-01 21:57:19 -0700

    Add lottie document.

---
+++ lottie_document.cpp
@@ -0,0 +1,2 @@
+#include "lottie_document.h"
+#include "core/resources.h"
---
+++ lottie_document.h
@@ -0,0 +1,11 @@
+#pragma once
+
+class LottieDocument : public Resource {
+  GDCLASS(LottieDocument, Resource);
+
+protected:
+  static void _bind_methods() {}
+
+public:
+  LottieDocument() {}
+};
