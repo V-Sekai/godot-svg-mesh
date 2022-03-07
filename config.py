d6039733ce06b655822c51aebfbc94ab0b1da5c0
K. S. Ernest (iFire) Lee <ernest.lee@chibifire.com>
2022-03-06 16:20:00 -0800

    Update docs.

--- config.py
+++ config.py
@@ -1,5 +1,24 @@
 def can_build(env, platform):
     return True
 
+
 def configure(env):
     pass
+
+
+def get_doc_path():
+    return "doc_classes"
+
+
+def get_doc_classes():
+    return [
+        "VGColor",
+        "VGGradient",
+        "VGLinearGradient",
+        "VGMeshRenderer",
+        "VGPaint",
+        "VGPath",
+        "VGRadialGradient",
+        "VGRenderer",
+        "EditorSceneImporterSVG",
+    ]
